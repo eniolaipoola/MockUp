010f4b906da8934d68ae5ba48358ba69dbe90533
oluwabamise <eniolaipoola@gmail.com>
2018-04-12 05:16:32 +0100

    Attached second mockup file to an activity

--- app/src/main/java/com/example/ehnyn/designui/MainActivity.java
+++ app/src/main/java/com/example/ehnyn/designui/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ehnyn.designui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nextButton = (Button) findViewById(R.id.btnNext);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Mock up 2", Toast.LENGTH_SHORT).show();
+                Intent homeIntent = new Intent(MainActivity.this, Page2Activity.class);
+                startActivity(homeIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ehnyn/designui/Page2Activity.java
@@ -0,0 +1,13 @@
+package com.example.ehnyn.designui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Page2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mock_up2);
+    }
+}
